ba9fcb9a8b358218b8bd927052f05192f8f04ae0
emillyflix <00001107073704sp@al.educacao.sp.gov.br>
2024-11-28 20:57:37 -0300

    trabalho final

---
+++ perguntas .js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que são códigos na programação?',
+    'Códigos são as instruções escritas em linguagens de programação que dizem ao computador o que fazer.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é a Alura?',
+    'A Alura é uma plataforma de cursos online focada em tecnologia, design, negócios e marketing.'
+) 
+
+criaCartao(
+    'Programação',
+    'O que significa "HTML"?',
+    'HTML (HyperText Markup Language) é uma linguagem usada para criar a estrutura de páginas da web.'
+) 
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'É um espaço na memória onde armazenamos dados que podem ser usados e alterados ao longo do programa.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo',
+    'Um algoritmo é uma sequência de passos ou instruções finitas e bem definidas para resolver um problema ou realizar uma tarefa específica.'
+) 
